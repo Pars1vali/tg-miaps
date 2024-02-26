00dad8d5da5c3d084d8f28d6ab1b159092c8cd6c
Chumak Vladimir <parsival427@gmail.com>
2024-02-26 22:30:35 +0300

    game-pay

---
+++ script.js
@@ -0,0 +1,41 @@
+const tg = window.Telegram.WebApp;
+let url = 'https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot';
+
+function upBtnClick(){
+    alert("Up");
+}
+function downBtnClick(){
+    alert("down");
+}
+function rightBtnClick(){
+    alert("right");
+}
+function leftBtnClick(){
+    alert("left");
+}
+
+
+tg.MainButton.show();
+tg.MainButton.setText("Оплатить");
+tg.MainButton.onClick(() => {
+    var data = JSON.stringify(
+        {
+            "name": "name",
+            "desciption": "desciption",
+            "payload": "payload",
+            "currency": "currency",
+            "cost": "cost"
+        }
+    );
+    console.log("start");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Custom-Info", "getOpenInvoiceUrl");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            tg.openInvoice(xhr.responseText);
+        }
+    };
+    xhr.send(data);
+});
