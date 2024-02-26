b2600fb8d708b99f009565a02db794ae422c180d
Chumak Vladimir <parsival427@gmail.com>
2024-02-26 22:35:43 +0300

    center image

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 const tg = window.Telegram.WebApp;
 let url = 'https://d5dip6pritbe7tmoain3.apigw.yandexcloud.net/aibot';
 
-function upBtnClick(){
-    alert("Up");
-}
-function downBtnClick(){
+function downBtnClick() {
     alert("down");
 }
-function rightBtnClick(){
+function rightBtnClick() {
     alert("right");
 }
-function leftBtnClick(){
+function leftBtnClick() {
     alert("left");
+}
+function upBtnClick() {
+    alert("up");
 }
 
 
-tg.MainButton.show();
-tg.MainButton.setText("Оплатить");
-tg.MainButton.onClick(() => {
-    var data = JSON.stringify(
-        {
-            "name": "name",
-            "desciption": "desciption",
-            "payload": "payload",
-            "currency": "currency",
-            "cost": "cost"
-        }
-    );
-    console.log("start");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-Custom-Info", "getOpenInvoiceUrl");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            tg.openInvoice(xhr.responseText);
-        }
-    };
-    xhr.send(data);
-});
+
+    tg.MainButton.show();
+    tg.MainButton.setText("Оплатить");
+    tg.MainButton.onClick(() => {
+        var data = JSON.stringify(
+            {
+                "name": "name",
+                "desciption": "desciption",
+                "payload": "payload",
+                "currency": "currency",
+                "cost": "cost"
+            }
+        );
+        console.log("start");
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-Custom-Info", "getOpenInvoiceUrl");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                tg.openInvoice(xhr.responseText);
+            }
+        };
+        xhr.send(data);
+    });
